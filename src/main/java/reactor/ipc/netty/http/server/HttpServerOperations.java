e606b808f288c132bd53645810672fa8852507ee
Stephane Maldini <smaldini@pivotal.io>
2018-05-21 00:23:26 -0700

    WIP - minor tweak to pass the test Need to review the workflow and when to insert HTTP2 support Especially tests are failing due to automatic 101 upgrade: h2c

--- src/main/java/reactor/ipc/netty/http/server/HttpServerOperations.java
+++ src/main/java/reactor/ipc/netty/http/server/HttpServerOperations.java
@@ -43,6 +43,7 @@ import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMessage;
 import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
@@ -120,6 +121,11 @@ class HttpServerOperations extends HttpOperations<HttpServerRequest, HttpServerR
 			Object msg, boolean forwarded) {
 		super(c, listener);
 		Objects.requireNonNull(msg, "msg");
+		this.nettyResponse = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+		this.responseHeaders = nettyResponse.headers();
+		this.compressionPredicate = compressionPredicate;
+		this.forwarded = forwarded;
+
 		if (msg instanceof Http2HeadersFrame) {
 			Http2HeadersFrame reqHeaders = (Http2HeadersFrame) msg;
 			this.http2Headers = reqHeaders.headers();
@@ -146,12 +152,8 @@ class HttpServerOperations extends HttpOperations<HttpServerRequest, HttpServerR
 
 			chunkedTransfer(true);
 		}
-
-		this.nettyResponse = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-		this.responseHeaders = nettyResponse.headers();
-		this.compressionPredicate = compressionPredicate;
 		this.cookieHolder = Cookies.newServerRequestHolder(requestHeaders());
-		this.forwarded = forwarded;
+
 	}
 
 	@Override
@@ -471,6 +473,7 @@ class HttpServerOperations extends HttpOperations<HttpServerRequest, HttpServerR
 				if (data.isEndStream()) {
 					onInboundComplete();
 				}
+				return;
 			}
 			else if(msg instanceof Http2HeadersFrame) {
 				listener().onStateChange(this, ConnectionObserver.State.CONFIGURED);
@@ -479,11 +482,15 @@ class HttpServerOperations extends HttpOperations<HttpServerRequest, HttpServerR
 						super.onInboundNext(ctx, msg);
 					}
 				}
+				return;
+			}
+			else if (msg instanceof HttpObject) {
+				//bridged
 			}
 			else {
 				super.onInboundNext(ctx, msg);
+				return;
 			}
-			return;
 		}
 
 		//http1
